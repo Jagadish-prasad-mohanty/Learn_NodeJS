7b47e2c3ad14a52f98ee9fe85dc7910d90ba2bab
jagadish123 <mohantyjagadish123@gmail.com>
2022-01-12 07:37:03 +0530

    different routes

--- nodeJs-start/app.js
+++ nodeJs-start/app.js
@@ -1,18 +1,34 @@
 const http= require('http');
 // const route=require('./route')
 // console.log(route);
-
+const fs=require('fs');
 const express= require('express');
+const bodyParser=require('body-parser');
 
 const app= express();
+app.use(bodyParser.urlencoded());
 
-app.use((req,res,next)=>{
-    console.log("A middleware");
+app.use('/',(req,res,next)=>{
+    console.log("Allways happen");
     next();
 })
-app.use((req,res,next)=>{
+
+app.use('/add-product',(req,res,next)=>{
+    console.log("A middleware");
+    res.send("<form action='/products' method='POST'><input name='title'/><button type='submit'>submit</button></form>");
+    
+    // next();
+})
+app.use('/products',(req,res,next)=>{
+    console.log(req.body);
+    fs.writeFile('data.txt',req.body['title'],()=>{
+
+        res.redirect('/');
+    })
+})
+app.use('/',(req,res,next)=>{
     console.log("Another middleware");
-    res.send("<h1>Hi from middleware</h1>")
+    res.send("<h1>Hi from middleware (HOME)</h1>");
 })
 
 
